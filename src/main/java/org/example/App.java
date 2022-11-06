e79d650ede363595eb7df5355549a9785254a16d
DIN-GS <nikita.ivakin@nure.ua>
2022-11-06 12:27:15 +0200

    test and code

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,58 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) throws IOException{
         System.out.println( "New Matrix homework" );
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(reader.readLine());
+        int m = Integer.parseInt(reader.readLine());
+        int[][] mas = new int[n][m];
+
+        for (int i =0; i < mas.length; i++){
+            for (int j =0; j < mas[0].length; j++){
+                n = Integer.parseInt(reader.readLine());
+                mas[i][j] = n;
+            }
+
+        }
+
+        transponyvanya(mas);
+
+    }
+
+    public static int[][] transponyvanya(int[][] mas){
+        int[][] mas2 = new int[mas[0].length][mas.length];
+
+        for (int i = 0; i < mas.length; i++){
+            for (int j = 0; j < mas[0].length; j++){
+                System.out.print(mas[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < mas2.length; i++){
+            for (int j = 0; j < mas2[0].length; j++){
+                mas2[i][j] = mas[j][i];
+            }
+        }
+
+        for (int i = 0; i < mas2.length; i++){
+            for (int j = 0; j < mas2[0].length; j++){
+                System.out.print(mas2[i][j]+ " ");
+            }
+            System.out.println();
+        }
+
+        return mas2;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -16,5 +17,13 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testTransponyvanya() {
+        int[][] mas = {{1, 2, 3}, {4,5,6}};
+        int[][] mas2 = App.transponyvanya(mas);
+        int[][] mas3 = {{1,4},{2,5},{3,6}};
+        assertArrayEquals(mas3, mas2);
     }
 }
